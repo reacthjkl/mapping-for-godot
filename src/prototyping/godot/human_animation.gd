d463f652922bf07741061d7dee83776e47d482d2
Illia Soloviov <74905269+reacthjkl@users.noreply.github.com>
2025-02-11 00:53:00 +0100

    added controll of cast shadow property by clicking enter (switching between shadow only and visible model with shadow)

--- src/prototyping/godot/human_animation.gd
+++ src/prototyping/godot/human_animation.gd
@@ -1,11 +1,26 @@
 extends Node3D
 
+# The object whose cast_shadow property you want to toggle
+@export var object_to_toggle: Node3D # Reference to the object, set this in the editor
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$AnimationPlayer.play("ArmatureAction_001")
-
+	
+	if not object_to_toggle:
+		object_to_toggle = $Armature/Skeleton3D/Object_4
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	
+	# Check for the 'V' key press
+	if Input.is_action_just_pressed("toggle_cast_shadow"):
+		_toggle_cast_shadow()
+		
+# Toggle the cast_shadow property between SHADOWS_ON and SHADOWS_ONLY
+func _toggle_cast_shadow():
+	if object_to_toggle:
+		if object_to_toggle.cast_shadow == 1:  # SHADOWS_ON
+			object_to_toggle.cast_shadow = 3  # SHADOWS_ONLY
+		else:
+			object_to_toggle.cast_shadow = 1  # SHADOWS_ON
