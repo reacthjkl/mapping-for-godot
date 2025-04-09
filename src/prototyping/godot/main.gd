3a8e2c8bf5cd8444f582a4e2ff37d99ba9b55768
Illia Soloviov <74905269+reacthjkl@users.noreply.github.com>
2025-04-09 16:50:34 +0200

    door opening animation

--- src/prototyping/godot/main.gd
+++ src/prototyping/godot/main.gd
@@ -5,7 +5,10 @@ extends Node3D
 func _ready() -> void:
 	pass
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("ui_accept"):
+		$pillars_scene/AnimationPlayer.play("Cube-animAction")
+	elif Input.is_action_just_pressed("ui_cancel"):  # "ui_cancel" is mapped to R below
+		$pillars_scene/AnimationPlayer.play_backwards("Cube-animAction")
+	
