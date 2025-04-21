891a81ad19c63aec54dfc655dcb507f8e50d4561
Illia Soloviov <74905269+reacthjkl@users.noreply.github.com>
2025-04-21 12:40:44 +0200

    gave clearer names to wave controller and opening controller variables

--- src/prototyping/godot/location-2-scenes/rock-cellars-test-scene/brick_wall/bricks.gd
+++ src/prototyping/godot/location-2-scenes/rock-cellars-test-scene/brick_wall/bricks.gd
@@ -1,23 +1,23 @@
 extends Node3D
 
 @export var wave_controller_path: NodePath    # Path to WaveController node
-@export var opening_controller_path: NodePath    # Path to OpeningController node
+@export var opening_controller_path: NodePath # Path to OpeningController node
 
-var wc                                        # WaveController reference
-var oc                                        # OpeningController reference
+var waveController                            # WaveController reference
+var openingController                         # OpeningController reference
 
 func _ready() -> void:
 	if wave_controller_path != NodePath() and has_node(wave_controller_path):
-		wc = get_node(wave_controller_path)
+		waveController = get_node(wave_controller_path)
 		
 	if opening_controller_path != NodePath() and has_node(opening_controller_path):
-		oc = get_node(opening_controller_path)
+		openingController = get_node(opening_controller_path)
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey:
 		if event.pressed and not event.echo:
 			match event.keycode:
 				KEY_SPACE:
-					wc.play()         
+					waveController.play()         
 				KEY_SHIFT:             
-					wc.stop()
+					waveController.stop()
