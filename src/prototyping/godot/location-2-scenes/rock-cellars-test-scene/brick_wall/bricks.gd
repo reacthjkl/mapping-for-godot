7f520646ebf87cd6d855548d8227c378deee5c37
Illia Soloviov <74905269+reacthjkl@users.noreply.github.com>
2025-04-25 20:24:27 +0200

    added heart particles to brick wall scene, implemented heart particles controll by keyboard for testing purposes

--- src/prototyping/godot/location-2-scenes/rock-cellars-test-scene/brick_wall/bricks.gd
+++ src/prototyping/godot/location-2-scenes/rock-cellars-test-scene/brick_wall/bricks.gd
@@ -2,9 +2,11 @@ extends Node3D
 
 @export var wave_controller_path: NodePath    # Path to WaveController node
 @export var opening_controller_path: NodePath # Path to OpeningController node
+@export var heart_particles_path: NodePath         # Path to HeartParticles node
 
 var waveController                            # WaveController reference
 var openingController                         # OpeningController reference
+var heartParticles                         # OpeningController reference
 
 func _ready() -> void:
 	# get controllers
@@ -13,6 +15,9 @@ func _ready() -> void:
 		
 	if opening_controller_path != NodePath() and has_node(opening_controller_path):
 		openingController = get_node(opening_controller_path)
+		
+	if heart_particles_path != NodePath() and has_node(heart_particles_path):
+		heartParticles = get_node(heart_particles_path)
 		
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey:
@@ -26,3 +31,5 @@ func _input(event: InputEvent) -> void:
 					openingController.open()
 				KEY_C:
 					openingController.close()
+				KEY_P:
+					heartParticles.emitting = !heartParticles.emitting
