15766b7d88d529cb4f2fea6294b9eac199d0594b
kern-core <lena@kernia.de>
2025-05-12 20:39:02 +0200

    Waiting music added to fade & main

--- src/deliverable/lumina-pax/scripts/fade_controller.gd
+++ src/deliverable/lumina-pax/scripts/fade_controller.gd
@@ -11,6 +11,7 @@ signal lights_in_completed
 var original_light_energies: Dictionary = {}
 
 @export var _origami_love_player: AudioStreamPlayer3D
+@export var _waiting_music_player: AudioStreamPlayer3D
 
 func music_fade_out(duration):
 	var target_volume = -80.0
@@ -23,6 +24,18 @@ func music_fade_out(duration):
 		time += get_process_delta_time()
 	time = 0.0
 	_origami_love_player.stop() 
+	
+func waiting_music_fade_out(duration):
+	var target_volume = -60.0
+	var time = 0.0
+	var volume_now = _waiting_music_player.volume_db
+	while time < duration:
+		var t = time / duration  # t geht von 0 bis 1
+		_waiting_music_player.volume_db = lerp(volume_now, target_volume, t)  # Verwende lerp mit float
+		await get_tree().process_frame
+		time += get_process_delta_time()
+	time = 0.0
+	_waiting_music_player.stop() 
 	
 func lights_fade_out(duration):
 	var target_lights = {
@@ -46,8 +59,12 @@ func lights_fade_out(duration):
 	emit_signal("lights_out_completed")
 	
 func music_start(value):
-	reset_music(value)
+	_origami_love_player.volume_db = value
 	_origami_love_player.play()
+	
+func waiting_music_start(value):
+	_waiting_music_player.volume_db = value
+	_waiting_music_player.play()
 	
 func lights_fade_in(duration):
 	var time = 0.0
@@ -63,5 +80,5 @@ func lights_fade_in(duration):
 			time = 0.0
 	emit_signal("lights_in_completed")
 	
-func reset_music(value):
-	_origami_love_player.volume_db = value
+func reset_waiting_music(value):
+	_waiting_music_player.volume_db = value
