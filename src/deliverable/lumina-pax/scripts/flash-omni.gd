bf24dbb21013d683f635d5f62e8e61077b3e8b03
kern-core <lena@kernia.de>
2025-05-15 20:30:42 +0200

    Wall Flash in Godot

--- src/deliverable/lumina-pax/scripts/flash-omni.gd
+++ src/deliverable/lumina-pax/scripts/flash-omni.gd
@@ -2,9 +2,13 @@ extends OmniLight3D
 
 @export var fired = false
 
+#----Audio-------------
+@export var _wall_flash_player: AudioStreamPlayer3D
+
 
 func request_flash():
 	if not fired:
+		_wall_flash_player.play()
 		fired = true
 		var tween = create_tween()
 		tween.tween_property(self, "light_energy", 16.0, 1.0).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
